ef871b0cd1b6a8d2104f5a8c1793d37988f7a080
aravlo <alvaro.riquelme-hernandez.external@capgemini.com>
2024-02-05 12:43:01 +0100

    Soluciones al apartado de mantenimiento de sonarqube

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -1,4 +1,5 @@
 package com.github.jitpack;
+import java.util.logging.Logger;
 
 /**
  * Esto es una traducción
@@ -9,34 +10,30 @@ public class App
 {
     public static void main(String[] args)
     {
-            System.out.println(new App().greet("Mundo de las ramas"));
-            
-
+        // System.out.println(new App().greet("Mundo de las ramas"));
+        Logger logger = Logger.getLogger(App.class.getName());
+        logger.info(new App().greet("Mundo de las ramas"));
     }
 
     public String greet(String name) {
         return "Hola " + name;
     }
     //esta clase no hace absolutamente nada más que meter evidencias para sonar.
-    public void claseInutil(){
-        int a=1;
-        String b;
-
-        for(int i=0; i<100; i++){
-            System.out.println(" ");
-            if(i == 0 ){
-                //jamas se cumplirá esta condición
+    public void claseInutil() {
+        Logger logger = Logger.getLogger(App.class.getName());
+        for (int i = 0; i < 100; i++) {
+            logger.info(" ");
+            if (i == -1) {
+                // This condition will never be true
             }
-        System.out.println("Code Smells a tope!!! ");
         }
-        
-        for(int i=0; i<100; i++){
-            System.out.println(" ");
-            if(i== 0 ){
-            //jamas se cumplirá esta condición
-                }
-            
-             }
+        logger.info("Code Smells a tope!!! ");
+
+        for (int i = 0; i < 100; i++) {
+            logger.info(" ");
+            if (i == -1) {
+                // This condition will never be true
+            }
         }
     }
-
+}
